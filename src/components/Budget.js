23a3420e7ec80f833e6c6e062ba9065847621082
Micah-Black <117412390+Micah-Black@users.noreply.github.com>
2023-02-07 00:10:19 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,34 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import { Currency } from '../components/Currency';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+	const { currency,budget,dispatch,expenses } = useContext(AppContext);
 
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+	const changeBudget = (val)=>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+
+		
+		if(val<totalExpenses) {
+			alert("You cannot reduce the budget that is already allocated!");
+		} else {
+			dispatch({
+				type: 'SET_BUDGET',
+				payload: val,
+			})
+			}
+        if(budget>=20000) {
+            alert("You cannot exceed 20,000")
+            budget=20000;
+        }
+    }   
+	return (
+		<div className='alert alert-secondary'>
+		<span>{currency}</span>	<input type="number" step="10" max="20000" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
+		</div>
+	);
 };
 
 export default Budget;
